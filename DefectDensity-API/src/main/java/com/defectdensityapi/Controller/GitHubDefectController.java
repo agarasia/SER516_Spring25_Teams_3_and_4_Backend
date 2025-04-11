73567009af65be5a4a05f3e568659b7bc579d3dd
HiteshKolluru-asu <hkolluru@asu.edu>
2025-04-10 22:56:39 -0700

    Revert "Update GitHubDefectController.java"
    This reverts commit f9480434631031fa77e233a73bc4c4afbc8f000c.

--- DefectDensity-API/src/main/java/com/defectdensityapi/Controller/GitHubDefectController.java
+++ DefectDensity-API/src/main/java/com/defectdensityapi/Controller/GitHubDefectController.java
@@ -130,4 +130,12 @@ public class GitHubDefectController {
             return ResponseEntity.status(500).body(responseMap);
         }
     }
+
+
+    @GetMapping("/loc-mock")
+    public ResponseEntity<Map<String, Object>> mockLinesOfCodeApi() {
+        Map<String, Object> mockResponse = new HashMap<>();
+        mockResponse.put("totalLinesOfCode", locApiAdapter.getTotalLinesOfCode());
+        return ResponseEntity.ok(mockResponse);
+    }
 }
