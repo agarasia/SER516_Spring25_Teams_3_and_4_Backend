d704dd3ae8872cdca85abebeec611a763dd1616e
thrupthihm <thosahal@asu.edu>
2025-04-23 15:49:23 -0700

    Task-79:Removed current mongodb integration in lcom4
    Removed current mongodb integration in lcom4 metric service.

--- LCOM4/routers/lcom4_router.py
+++ LCOM4/routers/lcom4_router.py
@@ -4,7 +4,6 @@ from typing import Optional
 from services.lcom4_calculator import calculate_lcom4
 from services.project_parser import parse_java_files_in_dir
 from services.github_service import fetch_project_from_github, cleanup_dir
-from services.mongo_service import to_doc_id, store_lcom4_data_in_mongo, fetch_lcom4_data_from_mongo, get_benchmark_from_mongo, store_benchmark_in_mongo
 from datetime import datetime
 import time
 
@@ -44,21 +43,14 @@ async def calculate_lcom4_endpoint(
             lcom4_value = calculate_lcom4(cls)
             results.append({"class_name": cls.name, "score": lcom4_value})
 
-
-        history_data = fetch_lcom4_data_from_mongo(gitHubLink)
         current_timestamp = datetime.utcfromtimestamp(time.time()).isoformat() + "Z"
 
-        current_data = {
+        response = {
             "timestamp": current_timestamp,
-            "data": results,
-            "gitUniqueId": to_doc_id(gitHubLink)   
+            "data": results
         }
 
-        store_lcom4_data_in_mongo(gitHubLink, results)
-
-
-        return { "lcom4_history": history_data if history_data else [],
-                 "current_lcom4": current_data }
+        return response
 
     except Exception as e:
         # Wrap any exceptions in an HTTP 500
@@ -68,30 +60,3 @@ async def calculate_lcom4_endpoint(
         # Cleanup temp files and directories
         if temp_dir:
             cleanup_dir(temp_dir)
-
-@router.post("/benchmark")
-def store_labels_for_project(
-    sourceValue: str = Body(..., example="https://github.com/owner/repo"),
-    benchmark: float = Body(..., example=1)
-):
-    """
-    Stores custom benchmark -> user entered bench mark in mongo for a given repo URL.
-    """
-    try:
-        store_benchmark_in_mongo(sourceValue, benchmark)
-        return {"message": "benchmark stored successfully."}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@router.get("/benchmark")
-def store_labels_for_project(
-    sourceValue: str = Query(..., example="https://github.com/owner/repo")
-):
-    """
-    get benchmark -> benchmark from mongo for a given repo URL.
-    """
-    try:
-        benchmark = get_benchmark_from_mongo(sourceValue)
-        return {"lcom4_benchmark": benchmark}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
