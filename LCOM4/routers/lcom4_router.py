58323551535239968f439e189e80171f55cedeba
unallami <unallami@asu.edu>
2025-04-27 02:41:12 -0700

    1. updated code for accessing shared volume 2. deleted middleware_service,utilities as they are not required since fetch_repo is added in the shared_volume_service

--- LCOM4/routers/lcom4_router.py
+++ LCOM4/routers/lcom4_router.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, HTTPException, Form, Body, Query 
+from services.shared_volume_service import fetch_repo
+# ,clone_repo
+
+from fastapi import APIRouter, HTTPException, Form
 from typing import Optional
 
 from services.lcom4_calculator import calculate_lcom4
 from services.project_parser import parse_java_files_in_dir
-from services.shared_volume_service import fetch_project_from_shared_volume
-from flask import jsonify
 from datetime import datetime
 import time
-import git
 
 router = APIRouter()
 
@@ -28,26 +28,25 @@ async def calculate_lcom4_endpoint(
     Response:
       - JSON object with LCOM4 results for each Java class.
     """
+    temp_dir = None
 
-    # Get the project from Shared Volune
+    # Fetch or Unzip the project
     try:
         if not gitHubLink:
             raise HTTPException(status_code=400, detail="Please provide a GitHub URL in gitHubLink.")
-            
-        # Clone the GitHub repo to a temp directory
-        head_sha, repo_path = fetch_project_from_shared_volume(gitHubLink)
 
-        repo_dir = git.Repo(repo_path) 
-        
-        if isinstance(repo_dir, dict) and "error" in repo_dir:
-            return jsonify({"error": repo_dir["error"]}), 200
+        # headsh1, dir_path = clone_repo(gitHubLink)  /un comment this line and clone_repo for testing
+
+        # fetch the GitHub repo from shared volume
+        headsh, temp_dir = fetch_repo(gitHubLink)
 
         # Parse .java files and compute LCOM4
-        java_classes = parse_java_files_in_dir(repo_dir)
+        java_classes = parse_java_files_in_dir(temp_dir)
         results = []
         for cls in java_classes:
             lcom4_value = calculate_lcom4(cls)
             results.append({"class_name": cls.name, "score": lcom4_value})
+
 
         current_timestamp = datetime.utcfromtimestamp(time.time()).isoformat() + "Z"
 
@@ -61,4 +60,3 @@ async def calculate_lcom4_endpoint(
     except Exception as e:
         # Wrap any exceptions in an HTTP 500
         raise HTTPException(status_code=500, detail=str(e))
-    
